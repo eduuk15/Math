e4536425ea584934a721e5160cc287458877584d
Eduardo <eduardo@conectra.com.br>
2022-10-31 18:28:17 -0300

    feat: adiciona composable de multiplicacao

---
+++ src/composables/Multiplica.js
@@ -0,0 +1,123 @@
+// O QUE PRECISA?
+
+// 1) Identificar o que precisa ser multiplicado... Para isso, sempre que tiver um ponto antes, multiplicar e substituir
+
+// A equação é sempre espaçada por 1 espaço em branco, resultando num ARRAY
+
+// Esse array tem diversos elementos, entre eles o ['.'] - PONTO - que significa a multiplicação
+
+// Logo, todo elemento que estiver depois do ponto deve ser multiplicado pelo anterior do ponto
+// Exemplo: 4 . 2 => 2 está depois do ponto, logo deve ser realizado 2x4
+
+// E depois disso 4 . 2 resultará em 8
+
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// CASOS ESPECIAIS
+
+// 1) Quando parenteses com incognita
+
+// Quando parenteses com incognita, o parenteses não poderá ser substituído por um elemente único, e continuará sendo um array
+// Exemplo: ( 5x + 3 ) nunca poderá ser um elemento sozinho, string ou int na lógica do programa
+// Logo ele será um elemento do tipo array... [2, '.', ['(', 5x, '+', 3, ')']]
+// Cabe então ao Parenteses retirar os sinais, para então o Multiplica iterar sobre cada item do array
+
+// 2) Quando incognita
+
+// Quando incognita, multiplica os numeros e concatena com a incognita
+
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// PARAMETROS
+
+// 1) ARRAY QUE TERÁ OS ELEMENTOS A SEREM MULTIPLICADOS
+// 2) INCOGNITA PARA VALIDAR QUANDO O CASO ESPECIAL N2 DEVE SER REALIZADO
+
+// RETORNO
+
+// Retorna um objeto, com o array atualizado para próxima etapa
+
+// const lado = ['5', '.', '3', '.', '4', '+', '3']
+
+// const multiplica = (ladoDaEquacao) => {
+//   // Primeiro tenho que validar se tem ponto, se não nem faz nada
+//   if (ladoDaEquacao.indexOf('.') === -1) {
+//     return null
+//   }
+
+//   // Ok, tem ponto!!! Então agora tem que multiplicar os elementos e ir trocando
+
+//   ladoDaEquacao.forEach((element, index, array) => {
+//     if (array[index - 1] === '.') {
+//       const elementosMultiplicados = parseFloat(element) * parseFloat(array[index - 2])
+//       ladoDaEquacao[index] = elementosMultiplicados
+//       ladoDaEquacao.splice(array[index - 1])
+//       ladoDaEquacao.splice(array[index - 2])
+//     }
+//   })
+
+//   return ladoDaEquacao
+// }
+
+// const x = multiplica(lado)
+// console.log(x)
+
+const lado = [
+  '5',
+  '.',
+  '3',
+  '.',
+  '4',
+  '+',
+  '3',
+  '+',
+  '-9',
+  '.',
+  '4',
+  '.',
+  '-2'
+]
+const incognita = 'x'
+
+const multiplica = (ladoDaEquacao, incognita) => {
+  // Primeiro tenho que validar se tem ponto, se não nem faz nada
+  if (ladoDaEquacao.indexOf('.') === -1) {
+    return null
+  }
+
+  // Ok, tem ponto!!! Então agora tem que multiplicar os elementos e ir trocando
+  ladoDaEquacao.forEach((element, index) => {
+    if (ladoDaEquacao[index - 1] === '.') {
+      if (
+        element.indexOf(incognita) > -1 ||
+          toString(ladoDaEquacao[index - 2]).indexOf(incognita) > -1
+      ) {
+        const elementosMultiplicados =
+            parseFloat(element) * parseFloat(ladoDaEquacao[index - 2])
+        ladoDaEquacao[index] = elementosMultiplicados + incognita
+        ladoDaEquacao[index - 1] = ''
+        ladoDaEquacao[index - 2] = ''
+      }
+      const elementosMultiplicados =
+          parseFloat(element) * parseFloat(ladoDaEquacao[index - 2])
+      ladoDaEquacao[index] = elementosMultiplicados
+      ladoDaEquacao[index - 1] = ''
+      ladoDaEquacao[index - 2] = ''
+    }
+  })
+
+  const itemVazio = ''
+
+  let itensVazios = ladoDaEquacao.indexOf(itemVazio)
+
+  while (itensVazios >= 0) {
+    ladoDaEquacao.splice(itensVazios, 1)
+    itensVazios = ladoDaEquacao.indexOf(itemVazio)
+  }
+
+  return ladoDaEquacao
+}
+
+const x = multiplica(lado, incognita)
+console.log('aqui o resultado')
+console.log(x)
