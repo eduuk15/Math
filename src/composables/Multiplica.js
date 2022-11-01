c74873432c9716ba8f018763c603d06b47207d09
eduuk15 <98836125+eduuk15@users.noreply.github.com>
2022-11-01 15:44:46 -0300

    feat: adiciona validacao para quando elemento do tipo array

--- src/composables/Multiplica.js
+++ src/composables/Multiplica.js
@@ -2,14 +2,14 @@
 
 // 1) Identificar o que precisa ser multiplicado... Para isso, sempre que tiver um ponto antes, multiplicar e substituir
 
-// A equação é sempre espaçada por 1 espaço em branco, resultando num ARRAY
+// A equaï¿½ï¿½o ï¿½ sempre espaï¿½ada por 1 espaï¿½o em branco, resultando num ARRAY
 
-// Esse array tem diversos elementos, entre eles o ['.'] - PONTO - que significa a multiplicação
+// Esse array tem diversos elementos, entre eles o ['.'] - PONTO - que significa a multiplicaï¿½ï¿½o
 
 // Logo, todo elemento que estiver depois do ponto deve ser multiplicado pelo anterior do ponto
-// Exemplo: 4 . 2 => 2 está depois do ponto, logo deve ser realizado 2x4
+// Exemplo: 4 . 2 => 2 estï¿½ depois do ponto, logo deve ser realizado 2x4
 
-// E depois disso 4 . 2 resultará em 8
+// E depois disso 4 . 2 resultarï¿½ em 8
 
 // -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -17,10 +17,10 @@
 
 // 1) Quando parenteses com incognita
 
-// Quando parenteses com incognita, o parenteses não poderá ser substituído por um elemente único, e continuará sendo um array
-// Exemplo: ( 5x + 3 ) nunca poderá ser um elemento sozinho, string ou int na lógica do programa
-// Logo ele será um elemento do tipo array... [2, '.', ['(', 5x, '+', 3, ')']]
-// Cabe então ao Parenteses retirar os sinais, para então o Multiplica iterar sobre cada item do array
+// Quando parenteses com incognita, o parenteses nï¿½o poderï¿½ ser substituï¿½do por um elemente ï¿½nico, e continuarï¿½ sendo um array
+// Exemplo: ( 5x + 3 ) nunca poderï¿½ ser um elemento sozinho, string ou int na lï¿½gica do programa
+// Logo ele serï¿½ um elemento do tipo array... [2, '.', ['(', 5x, '+', 3, ')']]
+// Cabe entï¿½o ao Parenteses retirar os sinais, para entï¿½o o Multiplica iterar sobre cada item do array
 
 // 2) Quando incognita
 
@@ -30,22 +30,22 @@
 
 // PARAMETROS
 
-// 1) ARRAY QUE TERÁ OS ELEMENTOS A SEREM MULTIPLICADOS
+// 1) ARRAY QUE TERï¿½ OS ELEMENTOS A SEREM MULTIPLICADOS
 // 2) INCOGNITA PARA VALIDAR QUANDO O CASO ESPECIAL N2 DEVE SER REALIZADO
 
 // RETORNO
 
-// Retorna um objeto, com o array atualizado para próxima etapa
+// Retorna um objeto, com o array atualizado para prï¿½xima etapa
 
 // const lado = ['5', '.', '3', '.', '4', '+', '3']
 
 // const multiplica = (ladoDaEquacao) => {
-//   // Primeiro tenho que validar se tem ponto, se não nem faz nada
+//   // Primeiro tenho que validar se tem ponto, se nï¿½o nem faz nada
 //   if (ladoDaEquacao.indexOf('.') === -1) {
 //     return null
 //   }
 
-//   // Ok, tem ponto!!! Então agora tem que multiplicar os elementos e ir trocando
+//   // Ok, tem ponto!!! Entï¿½o agora tem que multiplicar os elementos e ir trocando
 
 //   ladoDaEquacao.forEach((element, index, array) => {
 //     if (array[index - 1] === '.') {
@@ -62,62 +62,62 @@
 // const x = multiplica(lado)
 // console.log(x)
 
-const lado = [
-  '5',
-  '.',
-  '3',
-  '.',
-  '4',
-  '+',
-  '3',
-  '+',
-  '-9',
-  '.',
-  '4',
-  '.',
-  '-2'
-]
-const incognita = 'x'
+const lado = ["5", ".", ["2x", "3"]];
+const incognita = "x";
 
 const multiplica = (ladoDaEquacao, incognita) => {
-  // Primeiro tenho que validar se tem ponto, se não nem faz nada
-  if (ladoDaEquacao.indexOf('.') === -1) {
-    return null
+  // Primeiro tenho que validar se tem ponto, se nï¿½o nem faz nada
+  if (ladoDaEquacao.indexOf(".") === -1) {
+    return null;
   }
 
-  // Ok, tem ponto!!! Então agora tem que multiplicar os elementos e ir trocando
+  // Ok, tem ponto!!! Entï¿½o agora tem que multiplicar os elementos e ir trocando
   ladoDaEquacao.forEach((element, index) => {
-    if (ladoDaEquacao[index - 1] === '.') {
+    if (
+      ladoDaEquacao[index - 1] === "." &&
+      typeof element !== Array &&
+      typeof ladoDaEquacao[index - 2] !== Array
+    ) {
       if (
         element.indexOf(incognita) > -1 ||
-          toString(ladoDaEquacao[index - 2]).indexOf(incognita) > -1
+        toString(ladoDaEquacao[index - 2]).indexOf(incognita) > -1
       ) {
         const elementosMultiplicados =
-            parseFloat(element) * parseFloat(ladoDaEquacao[index - 2])
-        ladoDaEquacao[index] = elementosMultiplicados + incognita
-        ladoDaEquacao[index - 1] = ''
-        ladoDaEquacao[index - 2] = ''
+          parseFloat(element) * parseFloat(ladoDaEquacao[index - 2]);
+        ladoDaEquacao[index] = elementosMultiplicados + incognita;
+        ladoDaEquacao[index - 1] = "";
+        ladoDaEquacao[index - 2] = "";
+      } else {
+        const elementosMultiplicados =
+          parseFloat(element) * parseFloat(ladoDaEquacao[index - 2]);
+        ladoDaEquacao[index] = elementosMultiplicados;
+        ladoDaEquacao[index - 1] = "";
+        ladoDaEquacao[index - 2] = "";
       }
-      const elementosMultiplicados =
-          parseFloat(element) * parseFloat(ladoDaEquacao[index - 2])
-      ladoDaEquacao[index] = elementosMultiplicados
-      ladoDaEquacao[index - 1] = ''
-      ladoDaEquacao[index - 2] = ''
+    } else if (ladoDaEquacao[index - 1] === "." && typeof element === Array) {
+      const i = ladoDaEquacao[index - 2];
+      element.forEach((elementN, index) => {
+        const elementosMultiplicados =
+          parseFloat(elementN) * parseFloat(ladoDaEquacao[i]);
+        element[index] = elementosMultiplicados + incognita;
+        element[index - 1] = "";
+        element[index - 2] = "";
+      });
     }
-  })
+  });
 
-  const itemVazio = ''
+  const itemVazio = "";
 
-  let itensVazios = ladoDaEquacao.indexOf(itemVazio)
+  let itensVazios = ladoDaEquacao.indexOf(itemVazio);
 
   while (itensVazios >= 0) {
-    ladoDaEquacao.splice(itensVazios, 1)
-    itensVazios = ladoDaEquacao.indexOf(itemVazio)
+    ladoDaEquacao.splice(itensVazios, 1);
+    itensVazios = ladoDaEquacao.indexOf(itemVazio);
   }
 
-  return ladoDaEquacao
-}
+  return ladoDaEquacao;
+};
 
-const x = multiplica(lado, incognita)
-console.log('aqui o resultado')
-console.log(x)
+const x = multiplica(lado, incognita);
+console.log("aqui o resultado");
+console.log(x);
